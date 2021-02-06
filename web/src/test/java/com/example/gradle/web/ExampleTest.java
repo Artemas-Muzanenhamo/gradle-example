f51889116629411733b149d917f24154881a4e08
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2021-02-06 21:23:09 +0000

    polish up

---
+++ web/src/test/java/com/example/gradle/web/ExampleTest.java
@@ -0,0 +1,12 @@
+package com.example.gradle.web;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ExampleTest {
+    @Test
+    void exampleTest() {
+        assertEquals(0, 0);
+    }
+}
